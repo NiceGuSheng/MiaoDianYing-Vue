eca4a8d370de17e847d8b27bbdab8707ff4ab02e
NiceGuSheng <1178616748@qq.com>
2019-07-11 16:08:00 +0800

    Vue 2.x

---
+++ src/views/Cinema/index.vue
@@ -0,0 +1,28 @@
+<template>
+    <div id="main">
+        <Header title="喵喵影院" />
+        <div id="content">
+            <CiList />
+        </div>
+        <TabBar />
+    </div>
+</template>
+
+<script>
+import Header from '@/components/Header';
+import TabBar from '@/components/TabBar';
+import CiList from '@/components/CiList';
+
+export default {
+    name : 'Cinema',
+    components : {
+        Header,
+        TabBar,
+        CiList
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ vue.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    devServer : {
+        proxy : {
+            '/api' : {
+                target : 'http://39.97.33.178',
+                changeOrigin : true
+            }
+        }
+    },
+    publicPath: './'
+}
